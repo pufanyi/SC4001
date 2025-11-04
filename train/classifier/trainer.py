6b9b1046c3d8341ce5993ef24ffe6635c2535e82
pufanyi <pufanyi@sensetime.com>
2025-11-04 11:37:01 +0800

    Refactor ClassifierTrainer to use PreTrainedModel and integrate optimizer and learning rate scheduler: update model type, implement optimizer creation, and add learning rate scheduler method for improved training configuration.

--- train/classifier/trainer.py
+++ train/classifier/trainer.py
@@ -1,13 +1,17 @@
+import torch
 from omegaconf import DictConfig
+from torch.optim.lr_scheduler import LambdaLR
+from transformers import PreTrainedModel
 
 from classifier.data.dataset import HFDataset
-from classifier.models import Model
+from train.optimizer.get_lr_scheduler import get_lr_scheduler
+from train.optimizer.get_optimizer import get_optimizer
 
 
 class ClassifierTrainer:
     def __init__(
         self,
-        model: Model,
+        model: PreTrainedModel,
         train_dataset: HFDataset,
         val_dataset: HFDataset,
         config: DictConfig,
@@ -17,8 +21,11 @@ class ClassifierTrainer:
         self.val_dataset = val_dataset
         self.config = config
 
-    def create_optimizer(self):
-        pass
+    def create_optimizer(self) -> torch.optim.Optimizer:
+        return get_optimizer(self.config, self.model)
+
+    def create_lr_scheduler(self) -> LambdaLR:
+        return get_lr_scheduler(self.config, self.optimizer)
 
     def train(self):
         pass
